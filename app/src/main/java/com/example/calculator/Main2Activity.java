87d6a4e6024eac73430f834d7877de944a28dfec
Nefor75 <nefor1975@gmail.com>
2019-06-26 19:36:40 +0300

    First commit

---
+++ app/src/main/java/com/example/calculator/Main2Activity.java
@@ -0,0 +1,124 @@
+package com.example.calculator;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2);
+
+        sp = new SoundPool(2, AudioManager.STREAM_MUSIC, 0);
+        soundbed = sp.load(this, R.raw.bed, 1);
+        soundKapla = sp.load(this, R.raw.kaplya, 1);
+
+    }
+
+    protected int checkedRadioButtonId;
+    protected int convertInIntTag;
+    protected int convertInIntEditBenz;
+    protected int convertInIntEditSmes;
+    protected double resultRound;
+    protected double resultRoundTag;
+    protected double result;
+    protected double resultCrossTag;
+    protected String resultString;
+    protected String resultStringTag;
+    protected String gasolineStringTag;
+    protected String gasolineString;
+    protected String smesPlust1;
+    protected EditText editSmes;
+    protected EditText editBenz;
+    protected long exit;
+    SoundPool sp;
+    protected int soundKapla;
+    protected int soundbed;
+
+    public void onClick(View view) {
+
+        editSmes = findViewById(R.id.editSmes);
+        editBenz = findViewById(R.id.editBenz);
+        TextView resultOil = findViewById(R.id.result_oil);
+        TextView resultGasoline = findViewById(R.id.result_gasoline);
+        TextView resultProporciya = findViewById(R.id.result_proporciya);
+        RadioGroup rG = findViewById(R.id.radiogroup);
+        checkedRadioButtonId = rG.getCheckedRadioButtonId();
+        RadioButton checkedButton = findViewById(checkedRadioButtonId);
+        LinearLayout llbottom = findViewById(R.id.llbottom);
+
+        if (view.getId() == R.id.расчет) {
+            if (editBenz.length() == 0 && editBenz.length() == 0) {
+                sp.play(soundbed, 1, 1, 0, 0, 1);
+                llbottom.setVisibility(View.INVISIBLE);
+                Toast.makeText(this, "Недостаточно данных", Toast.LENGTH_SHORT).show();
+            }
+
+            else if (editBenz.length() != 0 && editSmes.length() == 0) {
+
+                convertInIntEditBenz = Integer.parseInt(editBenz.getText().toString());
+                convertInIntTag = Integer.parseInt(checkedButton.getTag().toString());
+
+                    resultCrossTag = (double) convertInIntEditBenz / convertInIntTag;
+
+                    if (Double.isNaN(resultCrossTag) || Double.isInfinite(resultCrossTag)|| convertInIntEditBenz == 0) {
+                        sp.play(soundbed, 1, 1, 0, 0, 1);
+                        llbottom.setVisibility(View.INVISIBLE);
+                        Toast.makeText(this, "Недостаточно данных", Toast.LENGTH_SHORT).show();
+                    } else {
+                        resultRoundTag = Math.round(resultCrossTag * 10.0) / 10.0;
+                        resultStringTag = resultRoundTag + "";
+                        gasolineStringTag = editBenz.getText() + "";
+                        sp.play(soundKapla, 1, 1, 0, 0, 1);
+
+                        resultProporciya.setText(checkedButton.getText());
+                        resultGasoline.setText(gasolineStringTag);
+                        resultOil.setText(resultStringTag);
+                        llbottom.setVisibility(View.VISIBLE);
+                    }
+            }
+            else if (editSmes.length() != 0 && editBenz.length() != 0) {
+
+                convertInIntEditSmes = Integer.parseInt(editSmes.getText().toString());
+                convertInIntEditBenz = Integer.parseInt(editBenz.getText().toString());
+                result = (double) convertInIntEditBenz / convertInIntEditSmes;
+                    if (Double.isNaN(result) || Double.isInfinite(result) || convertInIntEditBenz == 0){
+                        sp.play(soundbed, 1, 1, 0, 0, 1);
+                        llbottom.setVisibility(View.INVISIBLE);
+                        Toast.makeText(this, "Недостаточно данных", Toast.LENGTH_SHORT).show();
+                    }else {
+                        resultRound = Math.round(result * 10.0) / 10.0;
+                        resultString = resultRound + "";
+                        smesPlust1 = "1:" + editSmes.getText();
+                        gasolineString = editBenz.getText() + "";
+                        sp.play(soundKapla, 1, 1, 0, 0, 1);
+
+                        resultProporciya.setText(smesPlust1);
+                        resultGasoline.setText(gasolineString);
+                        resultOil.setText(resultString);
+                        llbottom.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        }
+    @Override
+    public void onBackPressed(){
+        if (exit + 3000 > System.currentTimeMillis()){
+            super.onBackPressed();
+        }
+        else{
+            Toast.makeText(getBaseContext(),"Для выхода из приложения нажмите кнопку НАЗАД еще раз", Toast.LENGTH_SHORT).show();
+        }
+        exit = System.currentTimeMillis();
+    }
+}
+
---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.calculator;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    SoundPool sp;
+    protected int soundKapla;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sp = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+        soundKapla = sp.load(this, R.raw.kaplya, 1);
+    }
+
+
+
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.начать) {
+            sp.play(soundKapla, 1, 1, 0, 0, 1);
+            Intent intent = new Intent(this, Main2Activity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+}
